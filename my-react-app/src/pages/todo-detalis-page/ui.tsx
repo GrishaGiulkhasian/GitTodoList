9df2516a9ccb401187fe748114a52da5595077e9
GrishaGiulkhasian <gulhasangrigor@gmail.com>
2024-07-10 17:00:25 +0300

    ListTodo

---
+++ my-react-app/src/pages/todo-detalis-page/ui.tsx
@@ -0,0 +1,35 @@
+import { Result, Row } from "antd";
+import { TaskCard, taskModel } from "entities/task";
+import { ToggleTask } from "features/toggle-task";
+import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+export const TodoDetailsPage = observer(() => {
+    const {
+        store: { getTask, task, isLoading, taskError },
+    } = taskModel;
+
+    const { id } = useParams<{id: string}>();
+
+    useEffect(() => {
+        if (id) {
+            getTask(id);
+        }
+    }, [id])
+
+    return (
+        <Row justify='center' align='middle'>
+            {taskError ? (
+                <Result title={taskError} />
+            ) : (
+                <TaskCard 
+                title={`Task#${task?.id}`} 
+                text={task?.title || ''} 
+                loading={isLoading} 
+                actions={task ? [<ToggleTask todo={task} />] : undefined}
+                />
+            )}
+        </Row>
+    )
+});
